afc11e110a79a3230c8a7fb740a5b09c9f037909
Aleks Sobieraj <contact@aleks.tech>
2021-01-09 23:04:03 -0500

    Map of USA

--- src/index.js
+++ src/index.js
@@ -1,24 +1,68 @@
 import * as d3 from 'd3'
-import canada from './canada-geojson.json'
+import us from './usa-topojson.json'
+import * as topojson from 'topojson-client'
 
-var width = 900,
-    height = 600;
+const width = 975;
+const height = 610;
 
-var svg = d3.select("svg")
-    .attr("width", width)
-    .attr("height", height);
+const path = d3.geoPath()
 
-const projection =
-  d3
-    .geoMercator()
-    .scale(200) // zoom
-    .center([-96.28, 62.24])
+const zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
 
-var pathGenerator = d3.geoPath()
-  .projection(projection)
+const svg = d3.select("svg")
+    .attr("viewBox", [0, 0, width, height])
+    .on("click", reset);
 
-svg.selectAll("path")
-  .data(canada.features)
-  .enter().append("path")
-  .attr("class", "boroughs")
-  .attr("d", pathGenerator);
+const g = svg.append("g");
+
+const states = g.append("g")
+    .attr("fill", "#444")
+    .attr("cursor", "pointer")
+  .selectAll("path")
+  .data(topojson.feature(us, us.objects.states).features)
+  .join("path")
+    .on("click", clicked)
+    .attr("d", path);
+
+states.append("title")
+    .text(d => d.properties.name);
+
+g.append("path")
+    .attr("fill", "none")
+    .attr("stroke", "white")
+    .attr("stroke-linejoin", "round")
+    .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
+
+svg.call(zoom);
+
+function reset() {
+  states.transition().style("fill", null);
+  svg.transition().duration(750).call(
+    zoom.transform,
+    d3.zoomIdentity,
+    d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
+  );
+}
+
+function clicked(event, d) {
+  const [[x0, y0], [x1, y1]] = path.bounds(d);
+  event.stopPropagation();
+  states.transition().style("fill", null);
+  d3.select(this).transition().style("fill", "red");
+  svg.transition().duration(750).call(
+    zoom.transform,
+    d3.zoomIdentity
+      .translate(width / 2, height / 2)
+      .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
+      .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
+    d3.pointer(event, svg.node())
+  );
+}
+
+function zoomed(event) {
+  const {transform} = event;
+  g.attr("transform", transform);
+  g.attr("stroke-width", 1 / transform.k);
+}
